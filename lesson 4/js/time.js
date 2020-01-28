aec5852cb213eed836e63ff7adb4ca7876311192
cwide001 <59721682+cwide001@users.noreply.github.com>
2020-01-27 22:12:37 -0800

    Update time.js

--- lesson 4/js/time.js
+++ lesson 4/js/time.js
@@ -4,7 +4,7 @@ var d=new Date()
     var monthname=new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug",
                     "Sep","Oct","Nov","Dec")
     document.write(weekday[d.getDay()] + " ")
-    document.write(d.getDate() + ". ")
+    document.write(d.getDate() + ", ")
     document.write(monthname[d.getMonth()] + " ")
     document.write(d.getFullYear())
     var p = document.getElementById("day").innerHTML = d;
