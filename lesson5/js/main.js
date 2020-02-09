3251a9935827e190a2e4277a4489be61d3b5daa4
cwide001 <59721682+cwide001@users.noreply.github.com>
2020-02-08 20:56:06 -0800

    Update main.js

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -1,6 +1,3 @@
-const hambutton = document.getElementsByClassName('ham')[0];
-hambutton.addEventListener("click", toggleMenu, false);
-
 function toggleMenu() {
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-};
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
