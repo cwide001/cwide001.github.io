41e070e039a8401c7124cb198e1fe7898e0672c9
cwide001 <59721682+cwide001@users.noreply.github.com>
2020-02-13 20:10:57 -0800

    Update windchill.js

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,17 +1,17 @@
+
 var temp = parseFloat(document.getElementById ('Current').textContent);
-var speed = parseFloat(document.getElementById ('Wind Speed').textContent);
+var speed = parseFloat(document.getElementById ('WindSpeed').textContent);
 var output = Math.round(windChill(temp, speed))
 
 if(temp <= 50 && speed > 3) {
-    document.getElementById('Wind Chill').innerHTML = output + "&deg;F"
+    document.getElementById('WindChill').innerHTML = output + "&deg;F"
 }
 else {
-    document.getElementById('Wind Chill').innerHTML = "N/A"
+    document.getElementById('WindChill').innerHTML = "N/A"
 }
 
 
-
-//calculation and result rounded to 2 decimals
+//calculation
 
 function windChill(tempF, speed) {
 return 35.74 + (0.6215 * tempF) - (35.75*(speed**0.16)) + (0.4275*(tempF)*(speed**0.16));
