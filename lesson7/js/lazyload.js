9cca0668f3926249e1ec60b3f896a8355745e324
cwide001 <59721682+cwide001@users.noreply.github.com>
2020-02-20 19:39:41 -0800

    Update lazyload.js

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,9 +1,8 @@
-const imagesToLoad = document.querySelectorAll('[data-src]');
+const imagesToLoad = document.querySelectorAll("[data-src]");
 const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
+	image.setAttribute("src", image.getAttribute("data-src"));
+	image.onload = () => { image.removeAttribute("data-src");
+	};
 };
 
 if('IntersectionObserver' in window) {
