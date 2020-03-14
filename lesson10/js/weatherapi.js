c026ff0bf2e51f0f1372605bcb8d49c27283207e
cwide001 <59721682+cwide001@users.noreply.github.com>
2020-03-13 17:09:58 -0700

    Update weatherapi.js

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,16 +1,40 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=e13503e22a461e9043203aaf017f9f1d';
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=32b36f37051b2578eec25a9c85d88e8a"
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    const currentTemp = document.querySelector('#current-temp');
 
-    currentTemp.textContent = jsObject.main.temp;
+    /*document.getElementById('current-temp').textContent = jsObject.main.temp;*/
+    document.getElementById('currentTemp').textContent = Math.round(jsObject.main.temp);
+    document.getElementById('Temp').textContent = Math.round(jsObject.main.temp_max);
+    document.getElementById('humidity').textContent = Math.round(jsObject.main.humidity);
+    document.getElementById('windSpeed').textContent = Math.round(jsObject.wind.speed);
 
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    /*const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
     const desc = jsObject.weather[0].description;  // note how we reference the weather array
     document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('apiicon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('apiicon').setAttribute('alt', desc);
-  });
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);*/
+  
+/*Calculate Wind Chill*/
+const temp = document.getElementById('currentTemp').textContent;
+const wind = document.getElementById('windSpeed').textContent;
+
+var t = parseFloat(temp);
+var s = parseFloat(wind);
+
+var windChill;
+
+if (t>50 || s<3) {
+    windChill = "N/A"
+    document.getElementById('windChill').innerHTML = windChill;
+}
+else {
+    windChill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+    windChill = Math.round(windChill*10)/10;
+    document.getElementById('windChill').innerHTML = windChill + "&deg;";
+}
+
+    
+});
