708fb3b8abb170ad2e4cca2e65eeef68e544e760
cwide001 <59721682+cwide001@users.noreply.github.com>
2020-03-18 16:17:16 -0700

    Update weatherapi.js

--- lesson11/js/weatherapi.js
+++ lesson11/js/weatherapi.js
@@ -11,20 +11,20 @@ fetch(apiURL)
     document.getElementById('humidity').textContent = jsObject.main.humidity;
     document.getElementById('windSpeed').textContent = jsObject.wind.speed;
 
-const temp = document.getElementById('Temp').textContent;
+const temp = document.getElementById('currentTemp').textContent;
 const wind = document.getElementById('windSpeed').textContent;
 
 var t = parseFloat(temp);
 var s = parseFloat(wind);
 
-var windChill;
-
-if (t>50 || s<3) {
-    windChill = "N/A"
-    document.getElementById('windChill').innerHTML = windChill;
+if (temperature <= 50 && wind >= 3) {
+        windchill = Math.round(35.74 + (0.6215 * temperature) - (35.75*(wind**0.16)) + (0.4275*(temperature)*(wind**0.16)));
+        document.getElementById('windChill').innerHTML = windchill;
 }
 else {
-    windChill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
-    windChill = Math.round(windChill*10)/10;
-    document.getElementById('windChill').innerHTML = windChill + "&deg;";
+        document.getElementById('windChill').innerHTML = "N/A";
+
 }
+
+
+});
