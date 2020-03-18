584e8a267e5c376d38e22c9cbed1911f23c7ce17
cwide001 <59721682+cwide001@users.noreply.github.com>
2020-03-18 12:58:34 -0700

    Update json.js

--- lesson11/js/json.js
+++ lesson11/js/json.js
@@ -5,7 +5,7 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject); // temporary checking for valid response and data parsing
+    console.table(jsonObject); 
     const towns = jsonObject['towns'];
     let rowNumber = 0;
     for (let i = 0; i < towns.length; i++ ) {
