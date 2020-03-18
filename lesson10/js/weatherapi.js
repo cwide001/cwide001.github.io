6fc9d04408f237fb8bc14c06ee733f45232c744d
cwide001 <59721682+cwide001@users.noreply.github.com>
2020-03-18 15:28:03 -0700

    Update weatherapi.js

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -5,11 +5,10 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject);
 
-   
-    document.getElementById('currentTemp').textContent = Math.round(jsObject.main.temp);
-    document.getElementById('Temp').textContent = Math.round(jsObject.main.temp_max);
-    document.getElementById('humidity').textContent = Math.round(jsObject.main.humidity);
-    document.getElementById('windSpeed').textContent = Math.round(jsObject.wind.speed);
+   document.getElementById('currentTemp').textContent = jsObject.weather[0].main;
+    document.getElementById('Temp').textContent = Math.round(jsObject.main.temp);
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('windSpeed').textContent = jsObject.wind.speed;
 
 
 const temp = document.getElementById('currentTemp').textContent;
@@ -18,17 +17,13 @@ const wind = document.getElementById('windSpeed').textContent;
 var t = parseFloat(temp);
 var s = parseFloat(wind);
 
-var windChill;
-
-if (t>50 || s<3) {
-    windChill = "N/A"
-    document.getElementById('windChill').innerHTML = windChill;
+if (temperature <= 50 && wind >= 3) {
+        windchill = Math.round(35.74 + (0.6215 * temperature) - (35.75*(wind**0.16)) + (0.4275*(temperature)*(wind**0.16)));
+        document.getElementById('windChill').innerHTML = windChill;
 }
 else {
-    windChill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
-    windChill = Math.round(windChill*10)/10;
-    document.getElementById('windChill').innerHTML = windChill + "&deg;";
+        document.getElementById('windChill').innerHTML = "N/A";
 }
 
-    
+   
 });
