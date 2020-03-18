54a7c064cac9236ac7224938c745e3f7143be05e
cwide001 <59721682+cwide001@users.noreply.github.com>
2020-03-18 16:31:33 -0700

    Update json.js

--- lesson11/js/json.js
+++ lesson11/js/json.js
@@ -46,11 +46,47 @@ fetch(requestURL)
         
         
         document.querySelector('div.townInfo').appendChild(card); }
-        //document.querySelector('div.row' + [rowNumber]).appendChild(info);   }
 
         else {
             continue;
         }
+        const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
-        
-  }});
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+
+    const towns = jsonObject['towns'];
+
+  for (let i = 0; i < towns.length; i++ ) {
+    if (towns[i].name == 'Preston'){
+    let card = document.createElement('section');
+    let article = document.createElement('article');
+    let h2 = document.createElement('h2');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let p3 = document.createElement('p');
+    let p4 = document.createElement('p');
+    
+    h2.textContent = towns[i].name + ' Upcoming Events';
+    p1.textContent = towns[i].events[0];
+    p2.textContent = towns[i].events[1];
+    p3.textContent = towns[i].events[2];
+    p4.textContent = towns[i].events[3];
+  
+    article.appendChild(h2);
+    article.appendChild(p1);
+    article.appendChild(p2);
+    article.appendChild(p3);
+    article.appendChild(p4);
+    card.appendChild(article);
+    
+    document.querySelector('div.events').appendChild(card);
+    }
+  }
+  });
+
+
